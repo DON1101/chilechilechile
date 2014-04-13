fc7f02129be0ec6f3f92f8899c8f4fe6f3f93d0a
Guojing Tang <guojingtang.1988@gmail.com>
2014-04-13 13:02:29 +0800

    Add requirements txt.

--- handlers/hello_world.py
+++ handlers/hello_world.py
@@ -7,7 +7,7 @@ logger = logging.getLogger("transformers." + __name__)
 class HelloWorldHandler(RequestHandler):
     def get(self):
         template_name = "hello_world.html"
-        db = Connection("localhost", "chilechilechile", "root")
+        db = Connection("localhost", "chilechilechile", "root", "MS@don1988")
         articles = db.query("SELECT * FROM articles")
         kwargs = dict(articles=articles)
         super(HelloWorldHandler, self).render(
