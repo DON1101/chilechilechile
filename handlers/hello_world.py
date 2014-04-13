da7e0bdb139f8d4d05d611efa59614677b10c12d
Guojing Tang <guojingtang.1988@gmail.com>
2014-04-13 12:55:19 +0800

    Display articles titile.

--- handlers/hello_world.py
+++ handlers/hello_world.py
@@ -1,3 +1,4 @@
+from torndb import Connection
 from tornado.web import RequestHandler
 import logging
 logger = logging.getLogger("transformers." + __name__)
@@ -6,4 +7,10 @@ logger = logging.getLogger("transformers." + __name__)
 class HelloWorldHandler(RequestHandler):
     def get(self):
         template_name = "hello_world.html"
-        super(HelloWorldHandler, self).render(template_name)
+        db = Connection("localhost", "chilechilechile", "root")
+        articles = db.query("SELECT * FROM articles")
+        kwargs = dict(articles=articles)
+        super(HelloWorldHandler, self).render(
+            template_name,
+            **kwargs
+        )
