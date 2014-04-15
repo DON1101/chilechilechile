d8f552487ae41db8ff5cf58653d031ce2eb66419
Guojing Tang <guojingtang.1988@gmail.com>
2014-04-16 01:15:03 +0800

    Make article list page.

--- handlers/article.py
+++ handlers/article.py
@@ -8,12 +8,18 @@ logger = logging.getLogger("transformers." + __name__)
 class ArticleListHandler(RequestHandler):
     def get(self):
         template_name = "article_list.html"
+        day = self.get_argument("day", "")
+        cur_page = self.get_argument("page", "0")
+        num_per_page = 5
+        sql = """SELECT * FROM articles WHERE 'day'={0} ORDER BY time DESC
+                 LIMIT {1}, {2}
+              """.format(day, int(cur_page) * num_per_page, num_per_page)
+
         db = Connection(settings.DATABASE_SERVER,
                         settings.DATABASE_NAME,
                         settings.DATABASE_USER,
                         settings.DATABASE_PASSWORD)
-        day = self.get_argument("day", "")
-        articles = db.query("SELECT * FROM articles")
+        articles = db.query(sql)
         kwargs = dict(articles=articles)
         super(ArticleListHandler, self).render(
             template_name,
