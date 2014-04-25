e006c7a2cc24dd31231117b763ceda09f67593fb
Guojing Tang <guojingtang.1988@gmail.com>
2014-04-26 00:53:09 +0800

    Add header and footer.

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
+from tornado.web import RedirectHandler
 from handlers.article import ArticleListHandler
 from handlers.weixin import WeixinHandler
 
 url_patterns = [
     (r"/weixin/?$", WeixinHandler),
-    (r"/", ArticleListHandler),
+    (r"/articles/", ArticleListHandler),
+
+    (r"/", RedirectHandler, {"url": "/articles/"}),
 ]
