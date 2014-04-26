3ee093387b5b7bfbb59bea40e7de5b0a653a1bba
Guojing Tang <guojingtang.1988@gmail.com>
2014-04-26 23:00:50 +0800

    Add favicon.ico.

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 from tornado.web import RedirectHandler, StaticFileHandler
+import settings
 from handlers.article import ArticleListHandler
 from handlers.weixin import WeixinHandler
 
 url_patterns = [
-    (r'/static/(.*)', StaticFileHandler, {"path": "media"}),
+    # Favicon
+    (r"/(favicon.ico)", StaticFileHandler, {"path": "/images/favicon.ico"}),
+    (r'/static/(.*)', StaticFileHandler, {"path": settings.static_path}),
 
     (r"/weixin/?$", WeixinHandler),
     (r"/articles/", ArticleListHandler),
