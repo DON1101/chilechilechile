fb715be64e70aa9280ce85539090f68d9a677a0d
Guojing Tang <guojingtang.1988@gmail.com>
2014-04-26 22:49:17 +0800

    Add /static url mapping.

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
-from tornado.web import RedirectHandler
+from tornado.web import RedirectHandler, StaticFileHandler
 from handlers.article import ArticleListHandler
 from handlers.weixin import WeixinHandler
 
 url_patterns = [
+    (r'/static/(.*)', StaticFileHandler, {"path": "media"}),
+
     (r"/weixin/?$", WeixinHandler),
     (r"/articles/", ArticleListHandler),
 
